c3c4fe242b6072140b63e41e33852c75769620a1
IZZY <451611304@qq.com>
2023-07-08 05:33:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "大腿",  // 同上...
-        "今天是你的生日",
+        "今天是你的生日哈哈",
         "这是我们在一起的",
         "第三个生日了哦",
         "去年的生日",
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-       // turn_on: "开始",
-       // play: "音乐",
-       // bannar_coming: "颜色",
-       // balloons_flying: "好像少点东西",
-       // cake_fadein: "蛋糕？",
-       // light_candle: "蜡烛？",
-       // wish_message: "生日快乐",
+        turn_on: "开始",
+        play: "音乐",
+       bannar_coming: "颜色",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
 };
